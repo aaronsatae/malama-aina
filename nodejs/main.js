f75730c4002fe95f19e6d5f930c4bcf9f5c511f4
Jihoon Park <chad9349@gmail.com>
2023-10-21 16:51:55 +0900

    more image

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,25 +3,21 @@ import sharp from 'sharp';
 import path from 'path';
 
 const __dirname = path.resolve();
-const imageBasePath = path.join(__dirname, '/../images/components')
+const imageBasePath = path.join(__dirname, '/../images')
+const resultBasePath = imageBasePath + '/result';
+const componentsBasePath = imageBasePath + '/components';
 
 const templateString = fs.readFileSync(path.join(__dirname, '/../templates/v1.svg')).toString();
 
-console.log(templateString);
-
 const excludeImageMap = {
-    '눈코1': new Set('얼굴장식2', '얼굴장식3'),
-    '눈코4': new Set('얼굴장식2', '얼굴장식3'),
-    '눈코7': new Set('얼굴장식2', '얼굴장식3'),
-    '머리카락2': new Set('얼굴장식1', '얼굴장식3', '눈썹4'),
-    '머리카락7': new Set('얼굴장식2'),
-    '얼굴장식2': new Set('입4', '입5', '입7', '입8', '입9', '페인팅1', '페인팅2', '페인팅3', '페인팅4',),
-    '얼굴장식3': new Set('입4', '입5', '입7', '입8', '입9',),
-    '페인팅43': new Set('옷3'),
-};
-
-function convertImageToBase64(image) {
-    return `data:image/png;base64,${image.toString('base64')}`;
+    'eyesAndNose1': ['faceAccessory2', 'faceAccessory3'],
+    'eyesAndNose4': ['faceAccessory2', 'faceAccessory3'],
+    'eyesAndNose7': ['faceAccessory2', 'faceAccessory3'],
+    'hair2': ['faceAccessory1', 'faceAccessory3', 'eyebrow4'],
+    'hair7': ['faceAccessory2'],
+    'faceAccessory2': ['mouth4', 'mouth5', 'mouth7', 'mouth8', 'mouth9', 'facePaint1', 'facePaint2', 'facePaint3', 'facePaint4',],
+    'faceAccessory3': ['mouth4', 'mouth5', 'mouth7', 'mouth8', 'mouth9',],
+    'facePaint4': ['clothes3'],
 }
 
 async function getImages(dir) {
@@ -31,7 +27,7 @@ async function getImages(dir) {
     for (const imgPath of imagePaths) {
         const image = await sharp(fs.readFileSync(`${dir}/${imgPath}`)).png().toBuffer();
         result.push({
-            imgName: imgPath.replace('.png', ''),
+            name: imgPath.replace('.png', ''),
             image: image,
         });
     }
@@ -39,78 +35,94 @@ async function getImages(dir) {
     return result;
 }
 
-const eyebrows = await getImages(path.join(imageBasePath, '눈썹'));
-const eyesAndNoses = await getImages(path.join(imageBasePath, '눈코'));
-const hairs = await getImages(path.join(imageBasePath, '머리카락'));
-const necklaces = await getImages(path.join(imageBasePath, '목장식'));
-const weapons = await getImages(path.join(imageBasePath, '무기'));
-const headAccessories = await getImages(path.join(imageBasePath, '얼굴장식'));
-const clothesList = await getImages(path.join(imageBasePath, '옷'));
-const mouths = await getImages(path.join(imageBasePath, '입'));
-const facePaints = await getImages(path.join(imageBasePath, '페인팅'));
 
-const hand = await sharp(fs.readFileSync(`${imageBasePath}/손.png`)).png().toBuffer();
-const body = await sharp(fs.readFileSync(`${imageBasePath}/몸통.png`)).png().toBuffer();
-const head = await sharp(fs.readFileSync(`${imageBasePath}/머리.png`)).png().toBuffer();
+function convertImageToBase64(image) {
+    return `data:image/png;base64,${image.toString('base64')}`;
+}
+
+function pickImage(images) {
+    return images[Math.floor(Math.random() * images.length)];
+}
+
+const eyebrows = await getImages(path.join(componentsBasePath, 'eyebrow'));
+const eyesAndNoses = await getImages(path.join(componentsBasePath, 'eyesAndNose'));
+const hairs = await getImages(path.join(componentsBasePath, 'hair'));
+const necklaces = await getImages(path.join(componentsBasePath, 'neckless'));
+const weapons = await getImages(path.join(componentsBasePath, 'weapon'));
+const headAccessories = await getImages(path.join(componentsBasePath, 'headAccessory'));
+const clothesList = await getImages(path.join(componentsBasePath, 'clothing'));
+const mouths = await getImages(path.join(componentsBasePath, 'mouth'));
+const facePaints = await getImages(path.join(componentsBasePath, 'facePaint'));
+
+const emptyImage = await sharp({
+    create: {
+        width: 1050,
+        height: 1050,
+        channels: 4,
+        background: {r: 0, g: 0, b: 0, alpha: 0.0}
+    }
+}).png().toBuffer();
+facePaints.push({name: '페인팅없음', image: emptyImage});
+necklaces.push({name: '목장식없음', image: emptyImage});
+headAccessories.push({name: '얼굴장식없음', image: emptyImage});
+
+const hand = await sharp(fs.readFileSync(`${componentsBasePath}/hand.png`)).png().toBuffer();
+const body = await sharp(fs.readFileSync(`${componentsBasePath}/body.png`)).png().toBuffer();
+const head = await sharp(fs.readFileSync(`${componentsBasePath}/head.png`)).png().toBuffer();
 
 const baseTemplate = templateString
     .replace(/{body}/g, convertImageToBase64(body))
     .replace(/{hand}/g, convertImageToBase64(hand))
     .replace(/{head}/g, convertImageToBase64(head));
 
-let imageCount = 0;
-for (const weapon of weapons) {
-    for (const mouth of mouths) {
-        for (const eyebrow of eyebrows) {
-            for (const eyesAndNose of eyesAndNoses) {
-                for (const hair of hairs) {
-                    for (const necklace of necklaces) {
-                        for (const clothes of clothesList) {
-                            for (const headAccessory of headAccessories) {
-                                for (const facePaint of facePaints) {
-                                    const combination = [
-                                        weapon.name,
-                                        eyebrow.name,
-                                        eyesAndNose.name,
-                                        hair.name,
-                                        mouth.name,
-                                        necklace.name,
-                                        clothes.name,
-                                        headAccessory.name,
-                                        facePaint.name,
-                                    ]
+const TOTAL_IMAGE_COUNT = 500;
+for (let imageCount = 0; imageCount < TOTAL_IMAGE_COUNT; imageCount++) {
+    const weapon = pickImage(weapons);
+    const mouth = pickImage(mouths);
+    const eyebrow = pickImage(eyebrows);
+    const eyesAndNose = pickImage(eyesAndNoses);
+    const hair = pickImage(hairs);
+    const necklace = pickImage(necklaces);
+    const clothes = pickImage(clothesList);
+    const headAccessory = pickImage(headAccessories);
+    const facePaint = pickImage(facePaints);
 
-                                    let needSkip = false;
-                                    for (const component of combination) {
-                                        excludeImageMap[component]?.forEach((excludeComponent) => {
-                                            if (combination.includes(excludeComponent)) {
-                                                needSkip = true;
-                                                break;
-                                            }
-                                        })
-                                    }
-                                    if (needSkip) {
-                                        continue;
-                                    }
+    const combination = [
+        weapon.name,
+        eyebrow.name,
+        eyesAndNose.name,
+        hair.name,
+        mouth.name,
+        necklace.name,
+        clothes.name,
+        headAccessory.name,
+        facePaint.name,
+    ];
 
-                                    imageCount += 1;
-                                    let tpl = baseTemplate
-                                        .replace(/{weapon}/g, convertImageToBase64(weapon.image))
-                                        .replace(/{eyebrow}/g, convertImageToBase64(eyebrow.image))
-                                        .replace(/{eyesAndNose}/g, convertImageToBase64(eyesAndNose.image))
-                                        .replace(/{hair}/g, convertImageToBase64(hair.image))
-                                        .replace(/{mouth}/g, convertImageToBase64(mouth.image))
-                                        .replace(/{necklace}/g, convertImageToBase64(necklace.image))
-                                        .replace(/{clothes}/g, convertImageToBase64(clothes.image))
-                                        .replace(/{headAccessory}/g, convertImageToBase64(headAccessory.image))
-                                        .replace(/{facePaint}/g, convertImageToBase64(facePaint.image))
-                                    await sharp(Buffer.from(tpl)).png().toFile(path.join(imageBasePath, `result/result${imageCount}.png`));
-                                }
-                            }
-                        }
-                    }
-                }
+    let needSkip = false;
+    for (const component of combination) {
+        const excludeComponents = excludeImageMap[component];
+        for (const c of combination) {
+            if (excludeComponents?.includes(c)) {
+                needSkip = true;
             }
         }
     }
+    if (needSkip) {
+        imageCount--;
+        console.log("skipped");
+        continue;
+    }
+    const tpl = baseTemplate
+        .replace(/{weapon}/g, convertImageToBase64(weapon.image))
+        .replace(/{eyebrow}/g, convertImageToBase64(eyebrow.image))
+        .replace(/{eyesAndNose}/g, convertImageToBase64(eyesAndNose.image))
+        .replace(/{hair}/g, convertImageToBase64(hair.image))
+        .replace(/{mouth}/g, convertImageToBase64(mouth.image))
+        .replace(/{necklace}/g, convertImageToBase64(necklace.image))
+        .replace(/{clothes}/g, convertImageToBase64(clothes.image))
+        .replace(/{headAccessory}/g, convertImageToBase64(headAccessory.image))
+        .replace(/{facePaint}/g, convertImageToBase64(facePaint.image))
+    sharp(Buffer.from(tpl)).png().toFile(path.join(resultBasePath, `result${imageCount}.png`));
+    console.log(`\rGenerated ${imageCount + 1} images! (${Math.floor((imageCount + 1) / TOTAL_IMAGE_COUNT * 100)}%)`);
 }
